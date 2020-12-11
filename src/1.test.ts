4c5675dbfa01f207136a8b42ec9ff0f90c1f03f9
sirxbiff@github.com <SirXbiff>
2020-12-11 09:37:46 +0100

    Finished day 1

--- src/1.test.ts
+++ src/1.test.ts
@@ -1,7 +1,7 @@
-import {find_pair_with_sum, readFileNumbers} from "./1";
+import {find_set_with_sum, set_2020_multiplication, readFileNumbers} from "./1";
 
 test('find example pair', () => {
-    let result = find_pair_with_sum(
+    let result = find_set_with_sum(
         [
             1721,
             979,
@@ -10,20 +10,44 @@ test('find example pair', () => {
             675,
             1456,
         ],
-        2020);
+        2020, 2);
 
     expect(result).toContain(1721)
     expect(result).toContain(299)
     expect(result.length).toBe(2)
 })
 
-test('find example answer', () => {
-    function pair_2020_multiplication(expenses: number[]) {
-        const pair_2020 = find_pair_with_sum(expenses, 2020)
-        return pair_2020[0] * pair_2020[1];
-    }
 
-    let result: number = pair_2020_multiplication(
+test('find example answer', () => {
+
+    let result: number = set_2020_multiplication([
+        1721,
+        979,
+        366,
+        299,
+        675,
+        1456,
+    ], 2);
+
+    expect(result).toBe(514579)
+})
+
+test('find example answer', () => {
+
+    let result: number = set_2020_multiplication([
+        1721,
+        979,
+        366,
+        299,
+        675,
+        1456,
+    ], 3);
+
+    expect(result).toBe(241861950)
+})
+
+test('find example triplet', () => {
+    let result = find_set_with_sum(
         [
             1721,
             979,
@@ -31,10 +55,30 @@ test('find example answer', () => {
             299,
             675,
             1456,
-        ]);
+        ],
+        2020, 3);
 
-    expect(result).toBe(514579)
+    expect(result.length).toBe(3)
+    expect(result).toContain(979)
+    expect(result).toContain(366)
+    expect(result).toContain(675)
 })
+
+test('find set of 3 with sum 2020', () => {
+
+    let result: number[] = find_set_with_sum([
+        20,
+        900,
+        1000,
+        1100
+    ], 2020, 3);
+
+    expect(result.length).toBe(3)
+    expect(result).toContain(20)
+    expect(result).toContain(900)
+    expect(result).toContain(1100)
+})
+
 
 test('read number from file', () =>{
     const expenses: number[] = readFileNumbers('data/1/input.txt')
--- src/1.ts
+++ src/1.ts
@@ -1,28 +1,38 @@
 import {readFileSync} from 'fs'
 
+export function set_2020_multiplication(expenses: NumberArray, set_size: number) {
+    const set_2020 = find_set_with_sum(expenses, 2020, set_size)
+    return set_2020.reduce((subresult, factor) => subresult * factor);
+}
+
+export function find_set_with_sum(terms: NumberArray, target_sum: number, set_size: number) {
+    let i: number = set_size - 1;
+    let set: number[] = []
+    while (i < terms.length && set.reduce((sum, entry) => sum + entry, 0) != target_sum || set.length != set_size) {
+        set[0] = terms[i];
+        if (set_size > 1) {
+            const subset = find_set_with_sum(terms.slice(0, i), target_sum - terms[i], set_size - 1)
+            set = [terms[i], ...subset]
+        }
+        i++;
+    }
+
+    return set
+}
+
+export function readFileNumbers(path: string) {
+    return readFileSync(path).toString().split('\n').map(Number)
+}
+
 interface NumberArray {
+    slice(start: number, end: number): NumberArray;
     length: number;
 
     [index: number]: number
 }
 
-export function find_pair_with_sum(terms: NumberArray, sum: number) {
-    let i: number = 0;
-    let pair: number[] = [0, 0]
-    while (i < terms.length && pair[0] + pair[1] != sum) {
-        pair[0] = terms[i];
-        let j: number = 0;
-        while (pair[0] + terms[j] != sum && j < i) {
-            j++;
-        }
-        pair[1] = terms[j];
-        i++;
-    }
 
-    return pair
-}
+const expenses: NumberArray = readFileNumbers('data/1/input.txt');
 
-export function readFileNumbers(path: string = '../data/1.ts') {
-    return readFileSync(path).toString().split('\n').map(Number)
-}
-
+console.log("First answer is: " + set_2020_multiplication(expenses,2))
+console.log("Second answer is: " + set_2020_multiplication(expenses,3))
